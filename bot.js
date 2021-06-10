38b6003c7e5fbc04c65f5c92678f3a220d478303
Kai Wang <kaiwang@Kais-Air.fios-router.home>
2021-06-10 16:06:21 -0400

    Add files

---
+++ bot.js
@@ -0,0 +1,31 @@
+const Discord = require('discord.js');
+
+const client = new Discord.Client();
+
+client.on('ready', () => {
+
+    console.log('I am ready!');
+
+});
+
+client.on('message', async message => {
+
+    var text = message.content;
+
+    if (text.includes('😂')) {
+
+       message.reply(message.author);
+
+       user = message.guild.member(message.author);
+
+       if (user.bannable) {
+
+          user.ban({ days: 7, reason: "That is what you get for being a normie"}).then(console.log).catch(console.error);
+
+       }
+
+     }
+
+});
+
+client.login(process.env.BOT_TOKEN);
